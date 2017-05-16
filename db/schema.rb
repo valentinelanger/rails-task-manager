ca11b499dfb5a94d6ee73697becd879dbbfd4bee
Valentine Langer <valentine.langer@gmail.com>
2017-05-16 11:23:22 +0200

    changed the name of my model for task

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516091235) do
+ActiveRecord::Schema.define(version: 20170516092246) do
 
-  create_table "todos", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
